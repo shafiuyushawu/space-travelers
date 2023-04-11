d78ed3e70ceac13bb8942b1e1f240e0843af30a3
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-04-11 09:55:15 +0000

    Added mission table

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,5 +1,56 @@
-import React from 'react';
-
-const Missions = () => <div>Missions</div>;
+const Missions = () => (
+  <section className=" px-10 py-5">
+    <table className="border w-full">
+      <thead>
+        <tr className="border">
+          <th className="border text-left p-2">Mission</th>
+          <th className="border text-left p-2">Description</th>
+          <th className="border text-left p-2">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="border">
+          <th className="border  p-2">THaicon</th>
+          <td className=" border p-2">
+            <p>
+              The Missions section displays a list of current missions along
+              with their brief description and participation status.
+              There is also a button next to each mission that allows users to join the
+              selected mission or leave the mission the user joined earlier.
+            </p>
+          </td>
+          <td className="border p-2"><span className="">Active Member</span></td>
+          <td className="border p-2">Blue</td>
+        </tr>
+        <tr className="border">
+          <th className="border  p-2">THaicon</th>
+          <td className=" border p-2">
+            <p>
+              The Missions section displays a list of current missions along
+              with their brief description and participation status.
+              There is also a button next to each mission that allows users to join the
+              selected mission or leave the mission the user joined earlier.
+            </p>
+          </td>
+          <td className="border p-2"><span className="">Active Member</span></td>
+          <td className="border p-2">Blue</td>
+        </tr>
+        <tr className="border">
+          <th className="border  p-2">THaicon</th>
+          <td className=" border p-2">
+            <p>
+              The Missions section displays a list of current missions along
+              with their brief description and participation status.
+              There is also a button next to each mission that allows users to join the
+              selected mission or leave the mission the user joined earlier.
+            </p>
+          </td>
+          <td className="border p-2"><span className="">Active Member</span></td>
+          <td className="border p-2">Blue</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+);
 
 export default Missions;
