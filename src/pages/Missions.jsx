49829768099cb488bed34e35c5bd540b2e518592
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-04-12 22:59:07 +0000

    Added loading component

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -2,14 +2,23 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMission } from '../redux/missions/missionSlice';
 import MissionList from '../components/MissionList';
+import Loading from '../components/Loading';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const missions = useSelector((state) => state.missions.missions);
+  const { missions, isLoading } = useSelector((state) => state.missions);
 
   useEffect(() => {
     if (missions.length === 0) dispatch(fetchMission());
   }, [dispatch, missions.length]);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center">
+        <Loading />
+      </div>
+    );
+  }
 
   return (
     <section className="px-10 py-5 ">
