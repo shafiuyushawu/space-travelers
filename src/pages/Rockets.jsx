0f07b166b2837a83cf12b33d3a9ca4074b4a97f5
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-12 14:51:17 +0000

    Move Rocket component into componts folder

--- src/pages/Rockets.jsx
+++ src/pages/Rockets.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
-import Rocket from './Rocket';
+import Rocket from '../components/Rocket';
 
 const Rockets = () => {
   const dispatch = useDispatch();
