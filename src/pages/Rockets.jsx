ba04c73e5968954e5860d2283f7926db0bbd58dc
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-12 02:55:30 +0000

    Dispatch an action to the store with fetched data

--- src/pages/Rockets.jsx
+++ src/pages/Rockets.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  
+  return 
   <div>Rockets</div>
-);
+}
 
 export default Rockets;
