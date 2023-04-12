8e292a0494079aa764c1337e4633d33a43426f74
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-12 13:57:09 +0000

    Add action dispatchers to the reservation buttons

--- src/pages/Rocket.jsx
+++ src/pages/Rocket.jsx
@@ -1,19 +1,34 @@
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
 
-const Rocket = ({ rocket }) => (
-  <div className="rocket">
-    <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
-    <div className="rocket-detail">
-      <h2>{rocket.rocket_name}</h2>
-      <p>{rocket.description}</p>
-      <Button variant="primary">Reserve Rocket</Button>
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+
+  const reserveRocket = () => {
+    dispatch({
+      type: 'RESERVE_ROCKET',
+      payload: {
+        rocketId: rocket.id,
+      },
+    });
+  };
+
+  return (
+    <div className="rocket">
+      <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+      <div className="rocket-detail">
+        <h2>{rocket.rocket_name}</h2>
+        <p>{rocket.description}</p>
+        <Button variant="primary" onClick={reserveRocket}>Reserve Rocket</Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
+    id: PropTypes.string,
     rocket_name: PropTypes.string,
     flickr_images: PropTypes.arrayOf(PropTypes.string),
     description: PropTypes.string,
