35cc948db9cabed403f62ee30772c11f5779e4ef
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-04-12 22:44:52 +0000

    Added notfound page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/layouts/NavBar';
 import Rockets from './pages/Rockets';
 import Mission from './pages/Missions';
 import Profile from './pages/Profile';
--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,10 +1,10 @@
 import { NavLink, Link } from 'react-router-dom';
-import logo from '../images/planet.png';
+import logo from '../../images/planet.png';
 
 const NavBar = () => (
-  <header>
+  <header className="mx-3 mb-5 border-b-2">
     <nav>
-      <Link href="/">
+      <Link to="/">
         <img className="logo" src={logo} alt="" />
         <h1>Space traveler&apos; Hub</h1>
       </Link>
