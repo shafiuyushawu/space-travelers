465cbbc8b0c34c5d2da2fe158b9ade98276aebd0
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-04-13 08:34:09 +0000

    Switch badges using conditional rendering

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import { useDispatch } from 'react-redux';
 import { reserveRocket } from '../redux/rockets/rocketsSlice';
 
@@ -15,8 +16,19 @@ const Rocket = ({ rocket }) => {
       <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
       <div className="rocket-detail">
         <h2>{rocket.rocket_name}</h2>
-        <p>{rocket.description}</p>
-        <Button variant="primary" onClick={handleReservation}>Reserve Rocket</Button>
+        <p>
+          {rocket.reserved && (
+          <Badge bg="info">
+            Reserved
+          </Badge>
+          )}
+          {rocket.description}
+        </p>
+        {
+          rocket.reserved
+            ? <Button variant="outline-secondary" onClick={handleReservation}>Reserve Rocket</Button>
+            : <Button variant="primary" onClick={handleReservation}>Reserve Rocket</Button>
+        }
       </div>
     </div>
   );
@@ -28,6 +40,7 @@ Rocket.propTypes = {
     rocket_name: PropTypes.string,
     flickr_images: PropTypes.arrayOf(PropTypes.string),
     description: PropTypes.string,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
 
